bf98ea82077f734093e7cc266678e79c7afbe1e9
mhclayt2 <mhclayt2@users.noreply.github.com>
2014-04-11 12:31:39 -0700

    Update dateCalculations.sql

--- dateCalculations.sql
+++ dateCalculations.sql
@@ -1,2 +1,4 @@
-datediff(hour,birth_date,GETDATE())/8766 as Age
-select dateadd(hour, datediff(hour, 0, @dt), 0)
+datediff(hour,birth_date,GETDATE())/8766 as Age -- calculate age
+
+
+select dateadd(hour, datediff(hour, 0, @dt), 0) -- truncate datetime to recent
