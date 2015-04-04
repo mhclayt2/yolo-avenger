6a99e1ae2bc8b338e35e19152d5810ebedb0dc8f
mhclayt2 <mhclayt2@gmail.com>
2015-04-04 09:15:40 -0700

    table sizes

--- Table Sizes.sql
+++ Table Sizes.sql
@@ -1,3 +1,23 @@
+select    
+      sum(reserved_page_count) * 8.0 / 1024
+from    
+      sys.dm_db_partition_stats
+
+GO
+
+select    
+      sys.objects.name, sum(reserved_page_count) * 8.0 / 1024
+from    
+      sys.dm_db_partition_stats, sys.objects
+where    
+      sys.dm_db_partition_stats.object_id = sys.objects.object_id
+
+group by sys.objects.name
+order by sum(reserved_page_count) * 8.0 / 1024 desc
+
+
+
+
 SELECT 
     t.NAME AS TableName,
     s.Name AS SchemaName,
